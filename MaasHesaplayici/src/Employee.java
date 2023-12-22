86a30d4fb7180e1adeab01da78c8d703e49002f6
Azadoz123 <azad.ozkaynak@gmail.com>
2023-12-22 17:41:25 +0300

    Maaş Hesaplama

--- MaasHesaplayici/src/Employee.java
+++ MaasHesaplayici/src/Employee.java
@@ -1,61 +1,54 @@
 public class Employee {
     String name;
-    double oldSalary;
     double salary;
     int workHours;
     int hireYear ;
-    double taxQuantity;
-    double bonusQuantity = 0;
-    double raiseSalaryQuantity = 0;
 
+    //Vergi miktarı hesaplama
     public double tax(double salary){
         if(salary < 1000){
-            taxQuantity = 0;
+            return 0;
         }else {
-            taxQuantity = salary * 3 / 100;
+            return salary * 3 / 100;
         }
-        return  taxQuantity;
     }
 
+    //Bonus hesaplama
     public double bonus(int workHours){
 
         if(workHours > 40){
-            bonusQuantity  = (workHours - 40) * 30;
+            return (workHours - 40) * 30;
         }
-        return bonusQuantity;
+        return 0;
     }
 
+    //Maaş artışı hesaplama
     public double raiseSalary(int hireYear, double salary){
         int year = 2021 - hireYear;
         if( year < 10){
-            raiseSalaryQuantity = salary * 5 /100;
+            return salary * 5 /100;
         }else if ( year > 9 && year < 20){
-            raiseSalaryQuantity = salary * 10 / 100;
+            return salary * 10 / 100;
         }else {
-            raiseSalaryQuantity = salary * 15 / 100;
+            return salary * 15 / 100;
         }
-        return raiseSalaryQuantity;
     }
+    // Bilgilerini stringe dönüştürme
     public String toString(){
         return "Adı: " + this.name + "\n"
-                + "Maaşı: " + this.oldSalary + "\n"
+                + "Maaşı: " + salary + "\n"
                 + "Çalışma Saati: " + this.workHours + "\n"
                 + "Başlangıç Yılı: " + this .hireYear + "\n"
-                + "Vergi: " + this.taxQuantity + "\n"
-                + "Bonus:" + this.bonusQuantity + "\n"
-                + "Maaş Artışı: " + this.raiseSalaryQuantity + "\n"
-                + "Vergi ve Bonuslarla ile birlikte maaş: " + (this.salary - this.raiseSalaryQuantity) + "\n"
-                + "Toplam maaş: " + this.salary;
+                + "Vergi: " + tax(salary) + "\n"
+                + "Bonus:" + bonus(workHours) + "\n"
+                + "Maaş Artışı: " + raiseSalary(hireYear,salary) + "\n"
+                + "Vergi ve Bonuslarla ile birlikte maaş: " + (salary - tax(salary) + bonus(workHours)) + "\n"
+                + "Toplam maaş: " + (salary - tax(salary) + bonus(workHours) + raiseSalary(hireYear, salary));
     }
     Employee(String name, double salary, int workHours, int hireYear){
         this.name = name;
-        this.oldSalary = salary;
         this.workHours = workHours;
         this.hireYear =hireYear;
-        this.taxQuantity = tax(salary);
-        this.bonusQuantity = bonus(workHours);
-        this.raiseSalaryQuantity = raiseSalary(hireYear, salary);
-        this.salary = salary - taxQuantity + bonusQuantity + raiseSalaryQuantity;
-
+        this.salary = salary;
     }
 }
--- MaasHesaplayici/src/Main.java
+++ MaasHesaplayici/src/Main.java
@@ -4,10 +4,9 @@ import java.util.Scanner;
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
+        // employee nesnesi oluşturuldu
         Employee employee = new Employee("Kemal", 2000,45,1985);
+        //Ekrana yazdırma
         System.out.println(employee.toString());
-        /*System.out.println("Adı: " + employee.name.toString() + "\n"
-                + "Maaşı: " + employee.salary
-                );*/
     }
 }
